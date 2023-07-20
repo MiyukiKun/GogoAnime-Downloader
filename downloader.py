bbcd317c0b273617d74efc6a77786e1bfef6904a
Majethia <hardikmajethia9529@gmail.com>
2023-07-20 23:35:26 +0530

    verify false

--- downloader.py
+++ downloader.py
@@ -6,7 +6,7 @@ def DownLoadFile(url, file_name="file.mkv"):
         os.remove(f"./downloads/{file_name}")
 
     chunk_size=1024*10
-    r = requests.get(url, allow_redirects=True, stream=True)
+    r = requests.get(url, allow_redirects=True, stream=True, verify=False)
     downloaded_size = 0
     with open(f"./downloads/{file_name}", 'wb') as fd:
         for chunk in r.iter_content(chunk_size=chunk_size):
